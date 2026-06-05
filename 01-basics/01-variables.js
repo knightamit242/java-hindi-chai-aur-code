0aa28d95a3f248c7ad4f58e4f9baa0d5070f93c5
Amit Singh <singhamit79449@gmail.com>
2026-06-05 21:27:59 +0530

    01-variable.js created

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,7 @@
+const accountId = 12345;
+
+
+// accountId = 54321; // This will throw an error because accountId is a constant
+console.log(accountId); // This will not be executed due to the error above
+
+
---
+++ 01-basics/011-variables.js
@@ -0,0 +1,9 @@
+const accountId = 12345 ;
+let accountEmail = "singhamit79449@gmail.com"
+var accountPassword="3434"
+accountCity="Delhi"
+
+console.log(accountId); // This will print 12345 to the console
+console.log(accountEmail); // This will print "singhamit79449@gmail.com" to the console
+console.log(accountPassword); // This will print "3434" to the console
+console.log(accountCity);

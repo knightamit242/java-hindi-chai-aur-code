6073d65925f313601005fbc01fe3bde0cc573ad2
Amit Singh <singhamit79449@gmail.com>
2026-06-06 13:35:29 +0530

    02-dataTypes.js created and updated

---
+++ 01-basics/02-dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //traet all js code as newer version
+
+//alert(3+3) //we are using nodejs not browser so we will use console.log instead of alert
+
+console.log(3+3) //6
+
+let name="Amit"
+let age=21
+let isLoggedIn=true
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+console.log(typeof name);   
+console.log(typeof "Amit")
+console.log(typeof age)
+console.log(typeof isLoggedIn)
+
+//object
+console.log(typeof null)
+
+console.log(typeof undefined)

049e667a2775903aff2e623cfa32f991cf87643d
Amit Singh <singhamit79449@gmail.com>
2026-06-07 12:12:39 +0530

    comparision of data tpes simple

--- 01-basics/04_comparision.js
+++ 01-basics/04_comparision.js
@@ -22,7 +22,7 @@
 // console.log(null==0)
 // console.log(null>=0)
 
-//**********************IMPORTANT***************************************//
+//***************************IMPORTANT***************************************//
 //The reason is that an equality check == and comparisons > < >= <= work differently.
 //Comparisons convert null to a number, treating it as 0.
 //That's why (3) null >= 0 is true and (1) null > 0 is false.

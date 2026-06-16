6bde1f3b62e5008a3ed8a239eec15ce6dd0d1093
Amit Singh <singhamit79449@gmail.com>
2026-06-16 14:06:28 +0530

    object destructure and definition of API

---
+++ 02-basics/05_object.js
@@ -0,0 +1,63 @@
+// SOME MORE ABOUT OBJECT 
+
+//1//
+// const course={
+
+//     courseName:"JavaFullStack",
+//     instructorName:"Hitesh Sir",
+//     coursePrice: 999
+// }
+
+// console.log(course.instructorName) // ONE WAY TO ACCESS AND WILL BE MOSTLY USED
+
+//ANOTHER WAY
+//1.1//
+
+// const course={
+
+//     courseName:"JavaFullStack",
+//     instructorName:"Hitesh Sir",
+//     coursePrice: 999
+// }
+
+//1.11//
+// const {instructorName}= course // EK BAAR YE DEFINE KR DO... PHIR BAAR-2 .instructorName NHI LIKHNA PADEGA
+
+// console.log(instructorName) // OUTPUT:Hitesh Sir 
+
+
+//1.12//
+// const {instructorName:ins}= course // ins=> AB AUR CHOTA HO GYA... WILL SAVE TIME AND USED IN REACT
+
+// console.log(ins)
+
+
+// OBJECT KO DESTRUCTURE KRNA ISSE BOLTE HAI
+// USED IN REACT
+
+
+
+
+//********************************API ****************************//
+
+
+// API(APPLICATION PROGRAMMING INTERFACE) SERVER SE DATA LANE KA TARIKA HAI.
+
+// API EK WAITER KI TARAH KAAM KARTI HAI.
+
+// AAJKAL API KA DATA ZYADATAR JSON FORMAT ME AATA HAI.
+
+// JSON = JAVASCRIPT OBJECT NOTATION
+
+// JSON OBJECT JAISA DIKHTA HAI.
+
+// JSON ME KEYS DOUBLE QUOTES ME HOTI HAIN.
+
+// EXAMPLE:
+
+// {
+//     "name": "Amit",
+//     "age": 22
+// }    
+
+
